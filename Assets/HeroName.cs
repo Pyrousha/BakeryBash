1ccb206aeafa527632a3e5edbf89bed07c511b74
Travis <tbird3@wisc.edu>
2022-01-04 16:08:31 -0600

    Hero Selection Multiplayer Test - Now Functional!

---
+++ Assets/HeroName.cs
@@ -0,0 +1,29 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroName : MonoBehaviour
+{
+    public SelectionPlayer player;
+    Text heroNameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        heroNameText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetText(string name)
+    {
+        //if (player.hasAuthority)
+        heroNameText.text = name;
+    }
+}
