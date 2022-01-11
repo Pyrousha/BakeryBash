b6944e9d7ce100eefa2193b53c6aa79a12d5d510
Aidan <expertsummer2002@gmail.com>
2022-01-11 16:47:33 -0600

    Menu with cool visual dynamics

---
+++ Assets/Scripts/UIMouseMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMouseMovement : MonoBehaviour
+{
+    public float multiplier;
+    
+    private Vector3 initialPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosClamped = new Vector3(Mathf.Clamp(Input.mousePosition.x, 0, Screen.width), Mathf.Clamp(Input.mousePosition.y, 0, Screen.height), Input.mousePosition.z);
+        Vector3 mousePosCentered = new Vector3(mousePosClamped.x - (Screen.width/2), mousePosClamped.y - (Screen.height/2), mousePosClamped.z);
+        transform.position = initialPosition - (mousePosCentered * multiplier);
+    }
+}
